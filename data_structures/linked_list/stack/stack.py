b263d7c2553d93b73e9c9a135ea123828dd6a8c1
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-03-30 16:42:29 -0700

    adding in ll

---
+++ data_structures/linked_list/stack/stack.py
@@ -0,0 +1,17 @@
+from node import Node
+
+class Stack:
+    def __init__(self, iterable=[])
+        self._current = None
+        self.head = None
+        self._size = 0
+
+        for item in reversed(iter):
+            self.insert(item)
+
+    def push(self, val)
+
+    def __len__(self):
+        return self._size
+
+    
