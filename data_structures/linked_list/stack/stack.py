4d43c1dfd5ed94ab1d19066945840191e13d2e74
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-03-30 17:29:26 -0700

    fixed linter error for init

--- data_structures/linked_list/stack/stack.py
+++ data_structures/linked_list/stack/stack.py
@@ -1,7 +1,8 @@
 from node import Node
 
 class Stack:
-    def __init__(self, iterable=[])
+    """Doc String"""
+    def __init__(self, iterable=[]):
         self._current = None
         self.head = None
         self._size = 0
@@ -9,7 +10,8 @@ class Stack:
         for item in reversed(iter):
             self.insert(item)
 
-    def push(self, val)
+    def push(self, val):
+        pass
 
     def __len__(self):
         return self._size
