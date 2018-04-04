636ed0585a1147627af69c124349d292af732916
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-03 17:38:18 -0700

    adding whiteboarding resubmit

--- challenges/ll_insertions/ll_insertions.py
+++ challenges/ll_insertions/ll_insertions.py
@@ -1,46 +1,17 @@
 from node import Node
+from linked_list import LinkedList as LL
 
-class LinkedList(object):
+class Insert(LL):
 
-    def __init__(self, iter = []):
+    def append(self, val):
 
-        self.head = None
-        self._size = 0
+        if self.head = None:
+            self.head = Node(self, val)
 
-        for val in iter:
-            self.head = Node(val, self.head)
-            self._size += 1
-
-    def __len__(self):
-        return self._size
-
-    def __str__(self):
-        el = []
-        current = self.head
-        for _ in range(self._size+1):
-            el.append(current)
-            current = current.next
-        return el
-
-    def insert(self):
-        self.head = Node(val, self.head)
-        self._size += 1
-
-
-    def find(self, val):
-        if self.head == None:
-            return False
-        elif self.head == val
-            return True
-        else:
-            current = self.head
-            cnext = current.next
-            if cnext.val == val:
-                return True
-            else:
-                while cNext.val != val and cnext is not None:
-                    current = cnext
-                    cNext = current.next
-                if cnext.val == val:
-                    return True
-                return False
+        cur = self.head
+        if not cur.next:
+            cur._next = Node(val)
+        while cur.next:
+            cur = cur.next
+        newnode = Node(val)
+        cur.next = newnode
