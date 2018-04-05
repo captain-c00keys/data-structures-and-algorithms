5420187cc2ab869b52c7befd9a08f62d3339c7f0
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-04 17:08:48 -0700

    no new changes

--- challenges/binary-search.py
+++
@@ -1,5 +0,0 @@
-def binary_search(array):
-    if len(array)>1:
-        array//2
-    else: len(array)<1:
-        return -1
--- challenges/shift-array.py
+++
@@ -1,5 +0,0 @@
-def insertShiftArray (array , value):
-    center = len(array)//2+1
-    for items in array:
-        value.append(center)
-        
