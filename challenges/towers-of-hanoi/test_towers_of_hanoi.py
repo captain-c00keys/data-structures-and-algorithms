215dc472b3b8e29b96725c5d172778de5d0f1810
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-05 23:43:07 -0700

    no new changes

--- challenges/towers-of-hanoi/test_towers_of_hanoi.py
+++ challenges/towers-of-hanoi/test_towers_of_hanoi.py
@@ -1,4 +1,4 @@
-import towers_of_hanoi
+from towers_of_hanoi import moveTower
 
 
 def test_move_disk_from_pole_a_to_pole_b():
--- challenges/towers-of-hanoi/towers_of_hanoi.py
+++ challenges/towers-of-hanoi/towers_of_hanoi.py
@@ -1,17 +1,18 @@
 def moveTower(n,fromPole, toPole, withPole):
-    # iterations = []
     if n >= 1:
         moveTower(n-1,fromPole,withPole,toPole)
         moveDisk(fromPole,toPole)
         moveTower(n-1,withPole,toPole,fromPole)
-    return
-
-def towers_of_hanoi(n):
-   return
 
 def moveDisk(fp,tp):
-    iterations = []
-    for n in moveTower:
-        towers_of_hanoi.append(iterations)
+    print("moving disk from",fp,"to",tp)
 
-# moveTower(3,"A","B","C")
+# def towers_of_hanoi(n):
+#    return
+
+# def moveDisk(fp,tp):
+#     iterations = []
+#     for n in moveTower:
+#         towers_of_hanoi.append(iterations)
+
+# # moveTower(3,"A","B","C")
