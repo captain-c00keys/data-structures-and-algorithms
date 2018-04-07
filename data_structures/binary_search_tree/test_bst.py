8676e406d1a284de7a4d01cfc6298bed883d0244
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-07 14:45:23 -0700

    still working on tests

--- data_structures/binary_search_tree/test_bst.py
+++ data_structures/binary_search_tree/test_bst.py
@@ -1,19 +1,17 @@
 from bst import BST
 import pytest
 
+def test_empty_tree(BST):
+    assert empty_bst is None
+    
 def test_insert():
     bst = BST()
     assert len(bst) == 0
     assert bst.insert(6).val == 6
     assert bst.current.val == 6
 
-@pytest.fixture
-def small_bst():
-    bst = BST([3,2,5])
-    return bst
+def test_insert_direction(small_bst):
+    assert pre_order == 5
 
-# def test_insert_direction(small_bst):
-#     assert pre_order == 5
-
-# def test_pre_order():
-#     assert _walk
+def test_pre_order():
+    assert _walk
