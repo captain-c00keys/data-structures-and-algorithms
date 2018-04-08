8469b30bfbefd25637f6d4a79d561fd9ad13843b
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-08 08:01:04 -0700

    passed two tests

--- challenges/ll_insertions/node.py
+++ challenges/ll_insertions/node.py
@@ -3,7 +3,7 @@ class Node(object):
     def __init__(self, val, _next=None):
         self.val = val
         self._next = next
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
 
     def __str__(self):
         return str(self.val)
