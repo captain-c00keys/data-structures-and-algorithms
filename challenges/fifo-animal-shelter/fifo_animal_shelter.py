bd26adf2f886bc486656510365d1bc39ad698715
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-14 12:40:08 -0700

    trying to finish animal shelter

---
+++ challenges/fifo-animal-shelter/fifo_animal_shelter.py
@@ -0,0 +1,20 @@
+from queue import Queue, Node
+
+class Dog:
+    pass
+
+class Cat:
+    pass
+
+class AnimalShelter:
+    def __init__(self, iter=[]):
+        self.pound = Queue(iter)
+        self.empty = Queue()
+
+    def enqueue(self, animal):
+        if type(animal) is not typeDog or Cat
+
+    
+
+
+tom = Cat()
