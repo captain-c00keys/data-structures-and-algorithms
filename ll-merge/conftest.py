72aac8b4b2fffe5b9ce3871d3b8c8bfb8e9243d6
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-15 11:42:33 -0700

    finished ll-merge

---
+++ ll-merge/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from .ll_merge import LinkedList as LL
+
+
+@pytest.fixture
+def empty_ll():
+    return LL()
+
+
+@pytest.fixture
+def small_ll():
+    return LL([1, 2, 3, 4])
+
+
+@pytest.fixture
+def short_ll():
+    return LL([5, 6, 7, 8])
+
+
+@pytest.fixture
+def long_ll():
+    return LL([11, 12, 13, 14, 15, 16])
