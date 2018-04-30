036d4f6be96d30dd6d0bcf8c512475f4c8db0b70
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-29 22:42:39 -0700

    finished hash table

---
+++ data_structures/hash_table/hash_table.py
@@ -0,0 +1,73 @@
+"""Hash table code."""
+INITIAL_CAPACITY = 50
+
+
+class Node:
+    """Node Class."""
+
+    def __init__(self, key, value):
+        """Empty Node."""
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+class HashTable(object):
+    """Hash Table."""
+
+    def __init__(self):
+        """Intialize hash table."""
+        self.capacity = INITIAL_CAPACITY
+        self.size = 0
+        self.buckets = [None] * self.capacity
+
+    def hash(self, key):
+        """Loops through keys."""
+        hashsum = 0
+        for idx, c in enumerate(key):
+            hashsum += (idx + len(key)) ** ord(c)
+            hashsum = hashsum % self.capacity
+        return hashsum
+
+    def insert(self, key, value):
+        """Insert node into buckets."""
+        self.size += 1
+        index = self.hash(key)
+        node = self.buckets[index]
+        if node is None:
+            self.buckets[index] = Node(key, value)
+            return
+        prev = node
+        while node is not None:
+            prev = node
+            node = node.next
+        prev.next = Node(key, value)
+
+    def find(self, key):
+        """Find node in buckets."""
+        index = self. hash(key)
+        node = self.buckets[index]
+        while node is not None and node.key != key:
+            node = node.next
+        if node is None:
+            return None
+        else:
+            return node.value
+
+    def delete(self, key):
+        """Delete node in buckets."""
+        index = self.hash(key)
+        node = self.buckets[index]
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+        if node is None:
+            return None
+        else:
+            self.size += 1
+            result = node.value
+            if prev is None:
+                node = None
+            else:
+                prev.next = prev.next.next
+            return result
