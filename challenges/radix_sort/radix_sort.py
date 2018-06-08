b8fecdc997f7a2fef934a68a7ddc6c6e9ec40a74
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-06-08 11:10:34 -0700

    finished radix sort

--- challenges/radix_sort/radix_sort.py
+++ challenges/radix_sort/radix_sort.py
@@ -1,29 +1,19 @@
-def radixsort(aList):
-    RADIX = 10
-    maxLength = False
-    tmp = -1
-    placement = 1
+import math
 
-    while not maxLength:
-        maxLength = True
-        # declare and initialize buckets
-        buckets = [list() for _ in range(RADIX)]
 
-        # split aList between lists
-        for i in aList:
-            import pdb; pdb.set_trace()
-            tmp = i / placement
-            buckets[tmp % RADIX].append(i)
-            if maxLength and tmp > 0:
-                maxLength = False
-
-        # empty lists into aList array
-        a = 0
-        for b in range(RADIX):
-            buck = buckets[b]
-            for i in buck:
-                aList[a] = i
-                a += 1
-
-        # move to next digit
-        placement *= RADIX
+def radix_sort(arr):
+    """Define radix sort."""
+    maxLen = -1
+    for num in arr:
+        numLen = int(math.log10(num)) + 1
+        if numLen > maxLen:
+            maxLen = numLen
+    for val in range(0, maxLen):
+        buckets = [[] for i in range(0, 10)]
+        for number in arr:
+            buckets[number // 10**val % 10].append(number)
+        arr = []
+        for bucket in buckets:
+            for num in bucket:
+                arr.append(num)
+    return arr
