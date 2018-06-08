9e5c4150f613c85a58b3a73ce772932770e7328a
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-06-08 11:32:13 -0700

    finished test selection

--- sorting_algos/selection.py
+++ sorting_algos/selection.py
@@ -1,13 +1,23 @@
-def sort(arr):
-    
-    lis = input
-    length = len(lis)
-    for i in range(length - 1):
-        for j in range(i+1, length):
-            if list[i] > lis[j]:
-                temp = lis[i]
-                lis[i] = lis[j]
-                lis[j] = temp
+def selection_sort(lst):
+    """Set up selection sort."""
+    arr = []
+    if len(arr) < 2:
+        return lst
 
-    return lis
+    def smallest_index(arr):
+        """Define helper function."""
+        i = arr[0]
+        sm_index = 0
+        for i in range(len(arr)):
+            if arr[i] < i:
+                i = arr[i]
+                sm_index = j
+        return sm_index
+
+    result = []
+    for j in range(len(arr)):
+        i = smallest_index(arr)
+        lst.append(arr.pop(i))
+
+    return lst
     
